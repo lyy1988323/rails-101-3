8892c61d27d76da5fd0b9454a212b5326d0cf47c
lee <lyy1988323@163.com>
2017-04-10 23:15:29 +0800

    update app/views/groups/index.html.erb

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,19 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def update
+     if @group.update(group_params)
+           redirect_to groups_path, notice: "Update Success"
+         else
+           render :edit
+         end
+  end
+
+   def destroy
+     @group.destroy
+     flash[:alert] = "Group deleted"
+     redirect_to groups_path
+   end
+   
 end
